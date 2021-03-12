6964e7d849da2f09ba9e2d6063dbe409c0e8a812
Roland Arsenault <roland@ccom.unh.edu>
2021-03-11 20:02:29 -0500

    Make topic names more generic

--- nodes/joy_to_helm.py
+++ nodes/joy_to_helm.py
@@ -61,9 +61,9 @@ if __name__ == '__main__':
     allow_differential_drive = rospy.get_param('allow_differential_drive', False)
     print ('allow_differential_drive:',allow_differential_drive)
     
-    helm_publisher = rospy.Publisher('manual/helm', Helm, queue_size=10)
+    helm_publisher = rospy.Publisher('helm', Helm, queue_size=10)
     if allow_differential_drive:
-      dd_publisher = rospy.Publisher('manual/differential_drive', DifferentialDrive, queue_size=10)
+      dd_publisher = rospy.Publisher('differential_drive', DifferentialDrive, queue_size=10)
     
     piloting_mode_publisher = rospy.Publisher('project11/send_command', String, queue_size=10)
     joy_subscriber = rospy.Subscriber('joy', Joy, joystickCallback)
